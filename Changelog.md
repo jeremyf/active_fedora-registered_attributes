820ecf707541877f737a5c32201635b57e6ea4f0
Justin Coyne <justin@curationexperts.com>
2013-10-03 07:52:11 -0500

    Bump version to 0.1.0.rc1

---
+++ Changelog.md
@@ -0,0 +1,3 @@
+# Changes
+## 0.1.0.rc1
+* Don't use deprecated options in ActiveFedora 6.6.0 [Justin Coyne]
--- lib/active_fedora/registered_attributes/version.rb
+++ lib/active_fedora/registered_attributes/version.rb
@@ -1,5 +1,5 @@
 module ActiveFedora
   module RegisteredAttributes
-    VERSION = "0.0.11"
+    VERSION = "0.1.0.rc1"
   end
 end
